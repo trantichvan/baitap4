9bfdf2b04570b9ed71046d56f577ee32396ff13a
aibtxhsu <149690311+aibtxhsu@users.noreply.github.com>
2023-11-07 01:39:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+pytest run in replit 
+ref https://replit.com/talk/learn/Python-unit-tests-with-PyTest/40976
---
+++ tests/test_00.py
@@ -0,0 +1,7 @@
+class Test:
+  
+  def test_00(self):
+    assert True 
+    
+  def test_01(self):
+    assert False, '# add your test logic here'
