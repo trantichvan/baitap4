8dbf01d79bc2b86d23949c257db3d6da64ad9495
namgivu <namgivu@gmail.com>
2023-11-07 04:06:48 +0000

    01b  goal is all test pass

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 """
 00 fork this replit to your replit 
-01 do your code
+
+01a do your code
+01b your final goal is to hit Run and have all tests PASS IN GREEN
 
 02a git commit push to github repo - view guide https://drive.google.com/file/d/1PZZ2xIlamM0pPtLlbpDodseCKcIVhTzW/view?usp=sharing
 02b get url to your git repo in 02a above - we call it :gitrepourl
